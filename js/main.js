be22bb500c48d81afb835d18f4daa985c04f17bc
LuizJarduli <luizjarduli@gmail.com>
2021-01-05 17:26:30 -0300

    Adicionando Avaliações e seus Icons

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         var busca = $('#name').val();
         //console.log(busca);
         var chave = '884b737f';
-        var request = "http://www.omdbapi.com/?apikey=" + chave + "&t=" + busca; // minha chave da API
+        var request = "http://www.omdbapi.com/?apikey=" + chave + "&t=" + busca + "&plot=full"; // minha chave da API
         $.ajax({
             method: 'GET',
             url: request,
@@ -30,6 +30,20 @@ $(document).ready(function() {
                     $("#duracao").text(data.Runtime);
                     $("#imdb").text(data.imdbRating);
                     $("#diretor").text(data.Director);
+                    $("#genero").text(data.Genre);
+                    $("#PG").text(data.Rated);
+                    $("#sinopse").text(data.Plot);
+                    $("#roteiro").text(data.Writer);
+                    $("#elenco").text(data.Actors);
+                    $("#rotten").append(
+                        "<img src='./img/rating-icons/certified-fresh-rotten.png' alt='Rotten Tomatoes'>&nbsp;&nbsp;" + data.Ratings[1].Value
+                    );
+                    $("#imdb2").append(
+                        "<img src='./img/rating-icons/IMDb-icon.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.imdbRating + "/10"
+                    );
+                    $("#metacritic").append(
+                        "<img src='./img/rating-icons/Metacritic-icon.png' alt='metacritic rating'>&nbsp;&nbsp;" + data.imdbRating + "/100"
+                    );
                 }
             },
             error: function(data) {
