ed0101fa1b169f6befcd10ffa9922f2b1a3f96e2
LuizJarduli <luizjarduli@gmail.com>
2021-01-05 19:44:43 -0300

    Corrigindo erros na URL

--- js/main.js
+++ js/main.js
@@ -1,80 +1,82 @@
 // no ato de carregar a pág, passo a queryString como parametro na requisição ajax
 window.onload = function(e) {
-        var queryString = window.location.search;
-        var busca = getParameterByName("title");
-        //console.log(queryString);
-        var chave = '884b737f';
-        var request = "http://www.omdbapi.com/?apikey=" + chave + "&t=" + busca + "&plot=full"; // minha chave da API
-        $.ajax({
-            method: 'GET',
-            url: request,
-            success: function(data) {
-                var card = $("#card");
-                $("#imdb2").html("");
-                $("#metacritic").html("");
-                $("#rotten").html("");
-                if (data.Response == "False") {
-                    alert("erro 404, filme não encontrado");
-                    if (card.is('.ativo')) {
-                        card.removeClass('ativo');
-                        card.addClass('hidden');
-                    }
-                } else {
-                    //console.log(data);
-                    if (card.is('.hidden')) {
-                        card.removeClass('hidden');
-                        card.addClass('ativo');
-                    }
+        if (window.location.pathname == "/filme.html") {
+            var queryString = window.location.search;
+            var busca = getParameterByName("title");
+            console.log(window.location);
+            var chave = '884b737f';
+            var request = "http://www.omdbapi.com/?apikey=" + chave + "&t=" + busca + "&plot=full"; // minha chave da API
+            $.ajax({
+                method: 'GET',
+                url: request,
+                success: function(data) {
+                    var card = $("#card");
+                    $("#imdb2").html("");
+                    $("#metacritic").html("");
+                    $("#rotten").html("");
+                    if (data.Response == "False") {
+                        alert("erro 404, filme não encontrado");
+                        if (card.is('.ativo')) {
+                            card.removeClass('ativo');
+                            card.addClass('hidden');
+                        }
+                    } else {
+                        //console.log(data);
+                        if (card.is('.hidden')) {
+                            card.removeClass('hidden');
+                            card.addClass('ativo');
+                        }
 
-                    //atribuindo o resultado na aplicação
-                    $("#poster").attr('src', data.Poster);
-                    $("#titulo").text(data.Title);
-                    $("#estreia").text(data.Year);
-                    $("#duracao").text(data.Runtime);
-                    $("#imdb").text(data.imdbRating);
-                    $("#diretor").text(data.Director);
-                    $("#genero").text(data.Genre);
-                    $("#PG").text(data.Rated);
-                    $("#sinopse").text(data.Plot);
-                    $("#roteiro").text(data.Writer);
-                    $("#elenco").text(data.Actors);
+                        //atribuindo o resultado na aplicação
+                        $("#poster").attr('src', data.Poster);
+                        $("#titulo").text(data.Title);
+                        $("#estreia").text(data.Year);
+                        $("#duracao").text(data.Runtime);
+                        $("#imdb").text(data.imdbRating);
+                        $("#diretor").text(data.Director);
+                        $("#genero").text(data.Genre);
+                        $("#PG").text(data.Rated);
+                        $("#sinopse").text(data.Plot);
+                        $("#roteiro").text(data.Writer);
+                        $("#elenco").text(data.Actors);
 
-                    //criei um condicional para os icones do rotten
-                    //existe o certified fresh para filmes com aceitação por 75% da crítica
-                    //fresh para 60% das críticas favoráveis
-                    //splat para filmes menores que 60%
-                    //also, criei uma segunda consição para evitar erros caso não exista registro do rotten no result da API
-                    if (parseInt(data.Ratings[1].Value) > 75 && data.Ratings[1].Source == "Rotten Tomatoes") {
-                        $("#rotten").append(
-                            "<img src='./img/rating-icons/certified-fresh-rotten.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.Ratings[1].Value
+                        //criei um condicional para os icones do rotten
+                        //existe o certified fresh para filmes com aceitação por 75% da crítica
+                        //fresh para 60% das críticas favoráveis
+                        //splat para filmes menores que 60%
+                        //also, criei uma segunda consição para evitar erros caso não exista registro do rotten no result da API
+                        if (parseInt(data.Ratings[1].Value) > 75 && data.Ratings[1].Source == "Rotten Tomatoes") {
+                            $("#rotten").append(
+                                "<img src='./img/rating-icons/certified-fresh-rotten.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.Ratings[1].Value
+                            );
+                        } else if (parseInt(data.Ratings[1].Value) > 60 && data.Ratings[1].Source == "Rotten Tomatoes") {
+                            $("#rotten").append(
+                                "<img src='./img/rating-icons/fresh-rotten.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.Ratings[1].Value
+                            );
+                        } else if (data.Ratings[1].Source == "Rotten Tomatoes") {
+                            $("#rotten").append(
+                                "<img src='./img/rating-icons/splat-rotten.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.Ratings[1].Value
+                            );
+                        }
+                        $("#imdb2").append(
+                            "<img src='./img/rating-icons/IMDb-icon.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.imdbRating + "/10"
                         );
-                    } else if (parseInt(data.Ratings[1].Value) > 60 && data.Ratings[1].Source == "Rotten Tomatoes") {
-                        $("#rotten").append(
-                            "<img src='./img/rating-icons/fresh-rotten.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.Ratings[1].Value
+                        $("#metacritic").append(
+                            "<img src='./img/rating-icons/Metacritic-icon.png' alt='metacritic rating'>&nbsp;&nbsp;" + data.Metascore + "/100"
                         );
-                    } else if (data.Ratings[1].Source == "Rotten Tomatoes") {
-                        $("#rotten").append(
-                            "<img src='./img/rating-icons/splat-rotten.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.Ratings[1].Value
-                        );
+                        $("#bilheteria").text(data.BoxOffice);
+                        $("#premios").text(data.Awards);
+                        $("#paises").text(data.Country);
+                        $("#idiomas").text(data.Language);
+                        $("#producao").text(data.Production);
                     }
-                    $("#imdb2").append(
-                        "<img src='./img/rating-icons/IMDb-icon.png' alt='Imdb rating'>&nbsp;&nbsp;" + data.imdbRating + "/10"
-                    );
-                    $("#metacritic").append(
-                        "<img src='./img/rating-icons/Metacritic-icon.png' alt='metacritic rating'>&nbsp;&nbsp;" + data.Metascore + "/100"
-                    );
-                    $("#bilheteria").text(data.BoxOffice);
-                    $("#premios").text(data.Awards);
-                    $("#paises").text(data.Country);
-                    $("#idiomas").text(data.Language);
-                    $("#producao").text(data.Production);
+                },
+                // em caso de erro na requisição, um alerta é exibido (será melhorado)
+                error: function(data) {
+                    alert("Ops, Ocorreu um erro com a requisição, Tente novamente");
                 }
-            },
-            // em caso de erro na requisição, um alerta é exibido (será melhorado)
-            error: function(data) {
-                alert("Ops, Ocorreu um erro com a requisição, Tente novamente");
-            }
-        })
+            })
+        }
 
     }
     //jQuery
